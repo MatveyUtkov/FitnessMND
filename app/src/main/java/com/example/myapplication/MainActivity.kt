92d59699bb501c8add0bcad15365fe7db0a469c9
MatveyUtkov <103751684+MatveyUtkov@users.noreply.github.com>
2023-01-05 00:16:01 +0600

    1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
@@ -44,5 +45,10 @@ class MainActivity : AppCompatActivity() {
         btnexercise!!.startAnimation(btthree)
         bgprogress!!.startAnimation(bttwo)
         bgprogresstop!!.startAnimation(lefttoright)
+        btnexercise!!.setOnClickListener {
+            val a = Intent(this@MainActivity, WorkoutAct::class.java)
+            a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(a)
+        }
     }
 }
